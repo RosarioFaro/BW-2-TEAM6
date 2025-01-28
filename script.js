754e482966631ba74dbe77ffbee304d731936106
LT-LuciaT <lucia.trombin98@gmail.com>
2025-01-28 10:29:41 +0100

    script

---
+++ script.js
@@ -0,0 +1,44 @@
+const URL = "https://striveschool-api.herokuapp.com/api/deezer/search?q={query}";
+
+function fetchAlbum(apiUrl, nvolte) {
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Errore nella richiesta: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((album) => {
+      console.log(album);
+
+      const randomObjects = album.data?.sort(() => Math.random() - 0.5).slice(0, nvolte);
+
+      if (randomObjects) {
+        const container = document.getElementById("consigliati");
+
+        randomObjects.forEach((album) => {
+          const cardDiv = document.createElement("div");
+          cardDiv.className = "col-2 p-0";
+
+          cardDiv.innerHTML = `
+            <div class="card">
+              <div class="imgCard">
+                <img src="${album.album.cover_medium}" class="card-img-top" alt="${album.title_short}" />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">${album.title}</h5>
+                <p class="card-text">${album.album.title}</p>
+              </div>
+            </div>
+          `;
+
+          container.appendChild(cardDiv);
+        });
+      } else {
+        console.error("Data is not in expected format or missing 'data' key.");
+      }
+    })
+    .catch((error) => {
+      console.error("Errore:", error.message);
+    });
+}
