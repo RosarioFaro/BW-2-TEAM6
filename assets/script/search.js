83c643fbaead5721cdef88b822afe1f16dde22fd
LT-LuciaT <lucia.trombin98@gmail.com>
2025-01-29 17:06:38 +0100

    search-artist

--- assets/script/search.js
+++ assets/script/search.js
@@ -22,20 +22,52 @@ function ricercaMusica() {
       risultatiContainer.innerHTML = "";
 
       const tracks = data.data.slice(0, 10);
+      const artists = {};
 
       tracks.forEach((track) => {
-        const trackElement = document.createElement("div");
-        trackElement.classList.add("result-item");
-        const trackContent = `
-          <img src="${track.album.cover}" alt="${track.title}" width="50" height="50">
-          <h4>${track.title}</h4>
-          <p>${track.artist.name}</p>
-          
-        `;
-
-        trackElement.innerHTML = trackContent;
-        risultatiContainer.appendChild(trackElement);
+        if (!artists[track.artist.id]) {
+          artists[track.artist.id] = {
+            name: track.artist.name,
+            id: track.artist.id,
+            tracks: [],
+          };
+        }
+        artists[track.artist.id].tracks.push(track);
       });
+
+      for (const artistId in artists) {
+        const artist = artists[artistId];
+
+        const artistElement = document.createElement("div");
+        artistElement.classList.add("artist-result");
+
+        const artistContent = `
+          <h3><a href="artist.html?id=${artist.id}">${artist.name}</a></h3>
+        `;
+        artistElement.innerHTML = artistContent;
+
+        // Ciclo sulle tracce dell'artista corrente
+        artist.tracks.forEach((track) => {
+          const trackElement = document.createElement("div");
+          trackElement.classList.add("result-item");
+
+          const trackContent = `
+            <img src="${track.album.cover}" alt="${track.title}" width="50" height="50">
+            <h4>${track.title}</h4>
+          `;
+
+          trackElement.innerHTML = trackContent;
+
+          trackElement.addEventListener("click", () => {
+            const albumID = track.album.id;
+            window.location.href = `album.html?id=${albumID}`;
+          });
+
+          artistElement.appendChild(trackElement);
+        });
+
+        risultatiContainer.appendChild(artistElement);
+      }
     })
     .catch((error) => {
       console.error("Errore nella ricerca:", error);
